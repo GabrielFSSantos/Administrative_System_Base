6deffe1299d99595d6afcbf97040bc3ead6c4a01
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2025-02-18 20:16:43 -0300

    Initial configurations (tsconfig, eslint)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,21 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'node_modules', 'dist'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
+      ecmaVersion: 'latest',
       sourceType: 'module',
       parserOptions: {
         projectService: true,
@@ -29,7 +27,20 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'no-new': 'off',
+      'no-useless-constructor': 'off',
+
+      // Regras similares às do @rocketseat/eslint-config/node
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'prefer-const': 'error',
+      'no-var': 'error',
+
+      // Adicionando regras do eslint-config-rocketseat/node.js (sem Prettier)
+      'arrow-parens': ['error', 'always'],
     },
   },
-);
+];
